e21a3a95180058f970cb75bd6aef98205547fa1e
maachs <maxvufa@yandex.ru>
2024-09-27 18:19:24 +0300

    qsort sring struct

--- Onegin.cpp
+++ Onegin.cpp
@@ -5,17 +5,21 @@
 #include <assert.h>
 #include <TXlib.h>
 
+struct Sort
+{
+    char* address;
+    int lineslen;
+};
 struct Inftext
 {
     int nlines;
     char* text;
-    char** address;
-    int* lineslen;
+    Sort* addr;
 };
 
 void SortText (Inftext* onegin);
 
-int Compare_lines (Inftext* onegin, const char* s1, const char* s2, int i);
+int Compare_lines (const char* s1, const char* s2, int len1, int len2);
 
 //void Swap_addr(char** swap1, char** swap2);
 
@@ -26,6 +30,8 @@ void ScanText(Inftext* onegin, unsigned long size_f);
 void PrintText(Inftext* onegin);
 
 void GetFileSize(Inftext* onegin, unsigned long size_f);
+
+int comp (const void* s1, const void* s2);
 
 int main()
 {
@@ -39,9 +45,7 @@ int main()
     onegin.nlines = 0;
 
     ScanText(&onegin, buffer.st_size);
-
-    onegin.address = (char**) calloc(onegin.nlines, sizeof(char*));
-    onegin.lineslen = (int*) calloc(onegin.nlines, sizeof(int));
+    onegin.addr = (Sort*) calloc(onegin.nlines, sizeof(Sort));
 
     GetFileSize(&onegin, buffer.st_size);
 
@@ -50,7 +54,7 @@ int main()
     PrintText(&onegin);
 
     free(onegin.text);
-    free(onegin.address);
+    free(onegin.addr);
 
     return 0;
 }
@@ -86,7 +90,7 @@ void PrintText(Inftext* onegin)
 {
     for (int ind = 0; ind < onegin->nlines; ind++)
     {
-        printf ("%s\n", (onegin->address)[ind]);
+        printf ("%s\n", (onegin->addr)[ind].address);
     }
 }
 
@@ -94,7 +98,7 @@ void GetFileSize(Inftext* onegin, unsigned long size_f)
 {
     int str = 1;
     int len = 0;
-    onegin->address[0] = onegin->text;
+    (onegin->addr)[0].address = onegin->text;
     for (unsigned int j = 0; j < size_f; j++)
     {
         if (onegin->text[j] == '\0')
@@ -103,8 +107,9 @@ void GetFileSize(Inftext* onegin, unsigned long size_f)
             {
                 break;
             }
-            (onegin->address) [str] = &(onegin->text)[j] + 1;
-            (onegin->lineslen)[len] = (onegin->address)[str] - (onegin->address)[str-1];
+            (onegin->addr)[str].address = &(onegin->text)[j] + 1;
+            (onegin->addr)[len].lineslen = (onegin->addr)[str].address - (onegin->addr)[str-1].address;
+
 
             //printf("%d\n", onegin->lineslen[len]);
             len++;
@@ -112,21 +117,29 @@ void GetFileSize(Inftext* onegin, unsigned long size_f)
             //printf("%d\n", index[str]);
         }
     }
+    (onegin->addr)[onegin->nlines-1].lineslen = &(onegin->text)[size_f] - (onegin->addr)[onegin->nlines-1].address;
+
+    /*for (int i = 0; i < onegin->nlines; i++)
+    {
+        printf("numb line %d len %d\n", i, (onegin->addr)[i].lineslen);
+    }*/
 }
 void SortText (Inftext* onegin)
 {
-    qsort(onegin->address, onegin->nlines, sizeof(char**), comp);
+    //printf("1");
+    qsort(onegin->addr, onegin->nlines, sizeof(Sort), comp);//sort lines len to address
+
     /*int str = onegin->nlines - 1;
 
     while (str > 0)
     {
         for (int i = 0; i < str ; i++)
         {
-            if (Compare_lines(onegin, (onegin->address)[i], (onegin->address)[i + 1], i) > 0)
+            if (Compare_lines(addr, (addr->address)[i], (addr->address)[i + 1], i) > 0)
             {
-                printf("swap 1 %s\nswap 2 %s\n", (onegin->address)[i], (onegin->address)[i + 1]);
-                Swap(&(onegin->address)[i], &(onegin->address)[i + 1], sizeof(char**));
-                Swap(&(onegin->lineslen)[i], &(onegin->lineslen)[i + 1], sizeof(int*));
+                //printf("swap 1 %s\nswap 2 %s\n", (addr->address)[i], (addr->address)[i + 1]);
+                Swap(&(addr->address)[i], &(addr->address)[i + 1], sizeof(char**));
+                Swap(&(addr->lineslen)[i], &(addr->lineslen)[i + 1], sizeof(int*));
             }
         }
         for (int ind = 0; ind < onegin->nlines; ind++)
@@ -138,15 +151,18 @@ void SortText (Inftext* onegin)
 
 }
 
-void comp (const char* s1, const char* s2)
+int comp (const void* elem1, const void* elem2)
 {
-
+    return Compare_lines((((const Sort*)elem1)->address), (((const Sort*)elem2)->address),
+                         (((const Sort*)elem1)->lineslen), (((const Sort*)elem2)->lineslen));
 }
-int Compare_lines (Inftext* onegin, const char* s1, const char* s2, int i)
-{
-    int elem1 = (onegin->lineslen)[i] - 1, elem2 = (onegin->lineslen)[i+1]-1;
 
-    printf ("compare line 1 %s\ncompare line 2 %s\n\n", s1, s2);
+int Compare_lines (const char* s1, const char* s2, int len1, int len2)
+{
+    int elem1 = len1 - 1 , elem2 = len2 - 1;
+    //printf ("len1 %d\nlen2 %d\n", len1, len2);
+
+    //printf ("compare line 1 %s\ncompare line 2 %s\n\n", s1, s2);
 
     while (elem1 >= 0 && elem2 >= 0)
     {
@@ -161,8 +177,8 @@ int Compare_lines (Inftext* onegin, const char* s1, const char* s2, int i)
 
         if (tolower(s1[elem1]) != tolower(s2[elem2]))
         {
-            printf("delta %d\n",tolower(s1[elem1]) - tolower(s2[elem2]) );
-            printf("%c %d\n%c %d\n", s1[elem1], onegin->lineslen[i] - elem1 - 2, s2[elem2], onegin->lineslen[i+1]-elem2 - 2);
+            //printf("delta %d\n",tolower(s1[elem1]) - tolower(s2[elem2]) );
+            //printf("%c %d\n%c %d\n", s1[elem1], len1 - elem1 - 2, s2[elem2], len2-elem2 - 2);
             return (tolower(s1[elem1]) - tolower(s2[elem2]));
         }
         else
